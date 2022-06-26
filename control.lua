780c00092cc80ef8da002d70fdaa5d8e6312a084
teapot4195 <huangalex409@gmail.com>
2022-06-26 08:16:31 -0400

    put a toggle switch on the hotbar

--- control.lua
+++ control.lua
@@ -21,43 +21,59 @@ local function swap(name, old, surface, player)
     return newBuilding
 end
 
+local Enabled = true
+
 local function updatePlayerXray(playerIndex)
-    local player = game.get_player(playerIndex)
-    local radius = 9
-    local surface = player.surface
-
-    local machines = surface.find_entities_filtered{position = player.position, radius = radius}
-
-    for _, machine in ipairs(machines) do
-        for _, Whitelisted in pairs(xrayBuildingWhitelist) do
-            if Whitelisted == machine.name then
-                local newMachine = swap(machine.name .. '-xray', machine, surface, player)
-
-                machine.destroy({raise_destroy = false})
-
-                if not global.players_xray[playerIndex] then
-                    global.players_xray[playerIndex] = {}
-                end
-                table.insert(global.players_xray[playerIndex], newMachine)
-            end
-        end
-    end
-
-    if global.players_xray[playerIndex] then
+    if Enabled == true then
         local player = game.get_player(playerIndex)
-        for index, machine in pairs(global.players_xray[playerIndex]) do
-            if machine.valid then
-                local dx = machine.position.x - player.position.x
-                local dy = machine.position.y - player.position.y
+        local radius = 9
+        local surface = player.surface
 
-                if dx * dx + dy * dy > radius * radius then
-                    swap(machine.name:sub(1, -6), machine, surface, player)
+        local machines = surface.find_entities_filtered{position = player.position, radius = radius}
+
+        for _, machine in ipairs(machines) do
+            for _, Whitelisted in pairs(xrayBuildingWhitelist) do
+                if Whitelisted == machine.name then
+                    local newMachine = swap(machine.name .. '-xray', machine, surface, player)
 
                     machine.destroy({raise_destroy = false})
 
+                    if not global.players_xray[playerIndex] then
+                        global.players_xray[playerIndex] = {}
+                    end
+                    table.insert(global.players_xray[playerIndex], newMachine)
+                end
+            end
+        end
+
+        if global.players_xray[playerIndex] then
+            local player = game.get_player(playerIndex)
+            for index, machine in pairs(global.players_xray[playerIndex]) do
+                if machine.valid then
+                    local dx = machine.position.x - player.position.x
+                    local dy = machine.position.y - player.position.y
+
+                    if dx * dx + dy * dy > radius * radius then
+                        swap(machine.name:sub(1, -6), machine, surface, player)
+
+                        machine.destroy({raise_destroy = false})
+
+                        global.players_xray[playerIndex][index] = nil
+                    end
+                else
                     global.players_xray[playerIndex][index] = nil
                 end
-            else
+            end
+        end
+    else
+        if global.players_xray[playerIndex] then
+            local player = game.get_player(playerIndex)
+            local surface = player.surface
+            for index, machine in pairs(global.players_xray[playerIndex]) do
+                if machine.valid then
+                    swap(machine.name:sub(1, -6), machine, surface, player)
+                    machine.destroy({raise_destroy = false})
+                end
                 global.players_xray[playerIndex][index] = nil
             end
         end
@@ -71,15 +87,24 @@ local function init()
 end
 
 script.on_init(
-  function()
-    init()
-  end
+    function()
+        init()
+    end
 )
 
 script.on_load(
-  function()
-    init()
-  end
+    function()
+        init()
+    end
+)
+
+script.on_event(defines.events.on_lua_shortcut,
+    function(event)
+        if event.prototype_name == "x-ray-toggle" then
+           Enabled = not Enabled
+           updatePlayerXray(event.player_index)
+        end
+    end
 )
 
 script.on_event(defines.events.on_player_changed_position,
---
+++ data.lua
@@ -0,0 +1,13 @@
+data:extend{
+    {
+      type = "shortcut",
+      name = "x-ray-toggle",
+      action = "lua",
+      icon = {
+        filename = "__Building-XRay__/placeholder.png",
+        width = 64,
+        height = 64,
+      },
+      toggleable = true,
+    },
+  }
